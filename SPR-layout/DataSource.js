9eae232fa8b72b399ca3b9b22e1257c6dd84d45f
foulsham <32790031+foulsham@users.noreply.github.com>
2018-10-30 15:21:08 +0000

    Add files via upload

---
+++ SPR-layout/DataSource.js
@@ -0,0 +1,19 @@
+
+var DataSourceA = [
+["A","10.newspaper.jpg","Landscape","Land1_10.newspaper.jpg","Land2_10.newspaper.jpg","Land3_10.newspaper.jpg","Land4_10.newspaper.jpg","Land5_10.newspaper.jpg","Land6_10.newspaper.jpg"],
+["A","10.newspaper.jpg","Portrait","Port1_10.newspaper.jpg","Port2_10.newspaper.jpg","Port3_10.newspaper.jpg","Port4_10.newspaper.jpg","Port5_10.newspaper.jpg","Port6_10.newspaper.jpg"],
+];
+
+
+var DataSourceB = [
+["B","10.newspaper.jpg","Landscape","Land1_10.newspaper.jpg","Land2_10.newspaper.jpg","Land3_10.newspaper.jpg","Land4_10.newspaper.jpg","Land5_10.newspaper.jpg","Land6_10.newspaper.jpg"],
+["B","10.newspaper.jpg","Portrait","Port1_10.newspaper.jpg","Port2_10.newspaper.jpg","Port3_10.newspaper.jpg","Port4_10.newspaper.jpg","Port5_10.newspaper.jpg","Port6_10.newspaper.jpg"],
+];
+
+
+var DataSourceC = [
+["C","10.newspaper.jpg","Landscape","Land1_10.newspaper.jpg","Land2_10.newspaper.jpg","Land3_10.newspaper.jpg","Land4_10.newspaper.jpg","Land5_10.newspaper.jpg","Land6_10.newspaper.jpg"],
+["C","10.newspaper.jpg","Portrait","Port1_10.newspaper.jpg","Port2_10.newspaper.jpg","Port3_10.newspaper.jpg","Port4_10.newspaper.jpg","Port5_10.newspaper.jpg","Port6_10.newspaper.jpg"],
+];
+
+var AllDataSources=[DataSourceA,DataSourceB,DataSourceC];
