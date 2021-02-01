6093892c3e88e17b3feb5a2a9d0d71c086c1a703
TF <foulsham@essex.ac.uk>
2021-02-01 14:20:52 +0000

    Add KO workings

---
+++ KOcomics/DataSource.js
@@ -0,0 +1,67 @@
+
+var DataSourceA = [
+["A",1,"13.rain_doghouse.jpg",24,"s1_L2x3_1.jpg","s1_L2x3_2.jpg","s1_L2x3_3.jpg","s1_L2x3_4.jpg","s1_L2x3_5.jpg","s1_L2x3_6.jpg","The dog found shelter by the end of the story.",2]
+];
+var DataSourceB = [
+["A",1,"13.rain_doghouse.jpg",24,"s1_L2x3_1.jpg","s1_L2x3_2.jpg","s1_L2x3_3.jpg","s1_L2x3_4.jpg","s1_L2x3_5.jpg","s1_L2x3_6.jpg","The dog found shelter by the end of the story.",2]
+];
+var DataSourceC = [
+["A",1,"13.rain_doghouse.jpg",24,"s1_L2x3_1.jpg","s1_L2x3_2.jpg","s1_L2x3_3.jpg","s1_L2x3_4.jpg","s1_L2x3_5.jpg","s1_L2x3_6.jpg","The dog found shelter by the end of the story.",2]
+];
+
+// var DataSourceB = [
+// ["B",1,"13.rain_doghouse.jpg",24,"s1_L3x2_1.jpg","s1_L3x2_2.jpg","s1_L3x2_3.jpg","s1_L3x2_4.jpg","s1_L3x2_5.jpg","s1_L3x2_6.jpg"],
+// ["B",2,"15.top.jpg",24,"s2_L3x2_1.jpg","s2_L3x2_2.jpg","s2_L3x2_3.jpg","s2_L3x2_4.jpg","s2_L3x2_5.jpg","s2_L3x2_6.jpg"],
+// ["B",3,"160.group_nap.jpg",24,"s3_L3x2_1.jpg","s3_L3x2_2.jpg","s3_L3x2_3.jpg","s3_L3x2_4.jpg","s3_L3x2_5.jpg","s3_L3x2_6.jpg"],
+// ["B",4,"220.SnoopyBall.jpg",24,"s4_L3x2_1.jpg","s4_L3x2_2.jpg","s4_L3x2_3.jpg","s4_L3x2_4.jpg","s4_L3x2_5.jpg","s4_L3x2_6.jpg"],
+// ["B",5,"234.ice_snoopy_knockdown.jpg",24,"s5_L3x2_1.jpg","s5_L3x2_2.jpg","s5_L3x2_3.jpg","s5_L3x2_4.jpg","s5_L3x2_5.jpg","s5_L3x2_6.jpg"],
+// ["B",6,"243.football_bonk.jpg",24,"s6_L3x2_1.jpg","s6_L3x2_2.jpg","s6_L3x2_3.jpg","s6_L3x2_4.jpg","s6_L3x2_5.jpg","s6_L3x2_6.jpg"],
+// ["B",7,"311.baseball.jpg",24,"s7_L3x2_1.jpg","s7_L3x2_2.jpg","s7_L3x2_3.jpg","s7_L3x2_4.jpg","s7_L3x2_5.jpg","s7_L3x2_6.jpg"],
+// ["B",8,"38.baseball_crash.jpg",24,"s8_L3x2_1.jpg","s8_L3x2_2.jpg","s8_L3x2_3.jpg","s8_L3x2_4.jpg","s8_L3x2_5.jpg","s8_L3x2_6.jpg"],
+// ["B",9,"178.s_leaf_steal.jpg",42,"s9_L3x2_1.jpg","s9_L3x2_2.jpg","s9_L3x2_3.jpg","s9_L3x2_4.jpg","s9_L3x2_5.jpg","s9_L3x2_6.jpg"],
+// ["B",10,"214.FootballSockKick.jpg",42,"s10_L3x2_1.jpg","s10_L3x2_2.jpg","s10_L3x2_3.jpg","s10_L3x2_4.jpg","s10_L3x2_5.jpg","s10_L3x2_6.jpg"],
+// ["B",11,"221.jumprope_snap.jpg",42,"s11_L3x2_1.jpg","s11_L3x2_2.jpg","s11_L3x2_3.jpg","s11_L3x2_4.jpg","s11_L3x2_5.jpg","s11_L3x2_6.jpg"],
+// ["B",12,"26.bobo_doll.jpg",42,"s12_L3x2_1.jpg","s12_L3x2_2.jpg","s12_L3x2_3.jpg","s12_L3x2_4.jpg","s12_L3x2_5.jpg","s12_L3x2_6.jpg"],
+// ["B",13,"272.paper_airplanes.jpg",42,"s13_L3x2_1.jpg","s13_L3x2_2.jpg","s13_L3x2_3.jpg","s13_L3x2_4.jpg","s13_L3x2_5.jpg","s13_L3x2_6.jpg"],
+// ["B",14,"43.piano_throw.jpg",42,"s14_L3x2_1.jpg","s14_L3x2_2.jpg","s14_L3x2_3.jpg","s14_L3x2_4.jpg","s14_L3x2_5.jpg","s14_L3x2_6.jpg"],
+// ["B",15,"50.odd_hit.jpg",42,"s15_L3x2_1.jpg","s15_L3x2_2.jpg","s15_L3x2_3.jpg","s15_L3x2_4.jpg","s15_L3x2_5.jpg","s15_L3x2_6.jpg"],
+// ["B",16,"54.gum_steal.jpg",42,"s16_L3x2_1.jpg","s16_L3x2_2.jpg","s16_L3x2_3.jpg","s16_L3x2_4.jpg","s16_L3x2_5.jpg","s16_L3x2_6.jpg"],
+// ["B",17,"105.li_punt.jpg",33,"s17_L3x2_1.jpg","s17_L3x2_2.jpg","s17_L3x2_3.jpg","s17_L3x2_4.jpg","s17_L3x2_5.jpg","s17_L3x2_6.jpg"],
+// ["B",18,"153.bad_meal.jpg",33,"s18_L3x2_1.jpg","s18_L3x2_2.jpg","s18_L3x2_3.jpg","s18_L3x2_4.jpg","s18_L3x2_5.jpg","s18_L3x2_6.jpg"],
+// ["B",19,"172.no_mitt.jpg",33,"s19_L3x2_1.jpg","s19_L3x2_2.jpg","s19_L3x2_3.jpg","s19_L3x2_4.jpg","s19_L3x2_5.jpg","s19_L3x2_6.jpg"],
+// ["B",20,"19.umbrella.jpg",33,"s20_L3x2_1.jpg","s20_L3x2_2.jpg","s20_L3x2_3.jpg","s20_L3x2_4.jpg","s20_L3x2_5.jpg","s20_L3x2_6.jpg"],
+// ["B",21,"23.newspaper_hat.jpg",33,"s21_L3x2_1.jpg","s21_L3x2_2.jpg","s21_L3x2_3.jpg","s21_L3x2_4.jpg","s21_L3x2_5.jpg","s21_L3x2_6.jpg"],
+// ["B",22,"248.snowfood.jpg",33,"s22_L3x2_1.jpg","s22_L3x2_2.jpg","s22_L3x2_3.jpg","s22_L3x2_4.jpg","s22_L3x2_5.jpg","s22_L3x2_6.jpg"],
+// ["B",23,"40.sugar_high.jpg",33,"s23_L3x2_1.jpg","s23_L3x2_2.jpg","s23_L3x2_3.jpg","s23_L3x2_4.jpg","s23_L3x2_5.jpg","s23_L3x2_6.jpg"],
+// ["B",24,"53.piano_slide.jpg",33,"s24_L3x2_1.jpg","s24_L3x2_2.jpg","s24_L3x2_3.jpg","s24_L3x2_4.jpg","s24_L3x2_5.jpg","s24_L3x2_6.jpg"]
+// ];
+// 
+// 
+// var DataSourceC = [
+// ["C",1,"13.rain_doghouse.jpg",24,"s1_L6x1_1.jpg","s1_L6x1_2.jpg","s1_L6x1_3.jpg","s1_L6x1_4.jpg","s1_L6x1_5.jpg","s1_L6x1_6.jpg"],
+// ["C",2,"15.top.jpg",24,"s2_L6x1_1.jpg","s2_L6x1_2.jpg","s2_L6x1_3.jpg","s2_L6x1_4.jpg","s2_L6x1_5.jpg","s2_L6x1_6.jpg"],
+// ["C",3,"160.group_nap.jpg",24,"s3_L6x1_1.jpg","s3_L6x1_2.jpg","s3_L6x1_3.jpg","s3_L6x1_4.jpg","s3_L6x1_5.jpg","s3_L6x1_6.jpg"],
+// ["C",4,"220.SnoopyBall.jpg",24,"s4_L6x1_1.jpg","s4_L6x1_2.jpg","s4_L6x1_3.jpg","s4_L6x1_4.jpg","s4_L6x1_5.jpg","s4_L6x1_6.jpg"],
+// ["C",5,"234.ice_snoopy_knockdown.jpg",24,"s5_L6x1_1.jpg","s5_L6x1_2.jpg","s5_L6x1_3.jpg","s5_L6x1_4.jpg","s5_L6x1_5.jpg","s5_L6x1_6.jpg"],
+// ["C",6,"243.football_bonk.jpg",24,"s6_L6x1_1.jpg","s6_L6x1_2.jpg","s6_L6x1_3.jpg","s6_L6x1_4.jpg","s6_L6x1_5.jpg","s6_L6x1_6.jpg"],
+// ["C",7,"311.baseball.jpg",24,"s7_L6x1_1.jpg","s7_L6x1_2.jpg","s7_L6x1_3.jpg","s7_L6x1_4.jpg","s7_L6x1_5.jpg","s7_L6x1_6.jpg"],
+// ["C",8,"38.baseball_crash.jpg",24,"s8_L6x1_1.jpg","s8_L6x1_2.jpg","s8_L6x1_3.jpg","s8_L6x1_4.jpg","s8_L6x1_5.jpg","s8_L6x1_6.jpg"],
+// ["C",9,"178.s_leaf_steal.jpg",42,"s9_L6x1_1.jpg","s9_L6x1_2.jpg","s9_L6x1_3.jpg","s9_L6x1_4.jpg","s9_L6x1_5.jpg","s9_L6x1_6.jpg"],
+// ["C",10,"214.FootballSockKick.jpg",42,"s10_L6x1_1.jpg","s10_L6x1_2.jpg","s10_L6x1_3.jpg","s10_L6x1_4.jpg","s10_L6x1_5.jpg","s10_L6x1_6.jpg"],
+// ["C",11,"221.jumprope_snap.jpg",42,"s11_L6x1_1.jpg","s11_L6x1_2.jpg","s11_L6x1_3.jpg","s11_L6x1_4.jpg","s11_L6x1_5.jpg","s11_L6x1_6.jpg"],
+// ["C",12,"26.bobo_doll.jpg",42,"s12_L6x1_1.jpg","s12_L6x1_2.jpg","s12_L6x1_3.jpg","s12_L6x1_4.jpg","s12_L6x1_5.jpg","s12_L6x1_6.jpg"],
+// ["C",13,"272.paper_airplanes.jpg",42,"s13_L6x1_1.jpg","s13_L6x1_2.jpg","s13_L6x1_3.jpg","s13_L6x1_4.jpg","s13_L6x1_5.jpg","s13_L6x1_6.jpg"],
+// ["C",14,"43.piano_throw.jpg",42,"s14_L6x1_1.jpg","s14_L6x1_2.jpg","s14_L6x1_3.jpg","s14_L6x1_4.jpg","s14_L6x1_5.jpg","s14_L6x1_6.jpg"],
+// ["C",15,"50.odd_hit.jpg",42,"s15_L6x1_1.jpg","s15_L6x1_2.jpg","s15_L6x1_3.jpg","s15_L6x1_4.jpg","s15_L6x1_5.jpg","s15_L6x1_6.jpg"],
+// ["C",16,"54.gum_steal.jpg",42,"s16_L6x1_1.jpg","s16_L6x1_2.jpg","s16_L6x1_3.jpg","s16_L6x1_4.jpg","s16_L6x1_5.jpg","s16_L6x1_6.jpg"],
+// ["C",17,"105.li_punt.jpg",33,"s17_L6x1_1.jpg","s17_L6x1_2.jpg","s17_L6x1_3.jpg","s17_L6x1_4.jpg","s17_L6x1_5.jpg","s17_L6x1_6.jpg"],
+// ["C",18,"153.bad_meal.jpg",33,"s18_L6x1_1.jpg","s18_L6x1_2.jpg","s18_L6x1_3.jpg","s18_L6x1_4.jpg","s18_L6x1_5.jpg","s18_L6x1_6.jpg"],
+// ["C",19,"172.no_mitt.jpg",33,"s19_L6x1_1.jpg","s19_L6x1_2.jpg","s19_L6x1_3.jpg","s19_L6x1_4.jpg","s19_L6x1_5.jpg","s19_L6x1_6.jpg"],
+// ["C",20,"19.umbrella.jpg",33,"s20_L6x1_1.jpg","s20_L6x1_2.jpg","s20_L6x1_3.jpg","s20_L6x1_4.jpg","s20_L6x1_5.jpg","s20_L6x1_6.jpg"],
+// ["C",21,"23.newspaper_hat.jpg",33,"s21_L6x1_1.jpg","s21_L6x1_2.jpg","s21_L6x1_3.jpg","s21_L6x1_4.jpg","s21_L6x1_5.jpg","s21_L6x1_6.jpg"],
+// ["C",22,"248.snowfood.jpg",33,"s22_L6x1_1.jpg","s22_L6x1_2.jpg","s22_L6x1_3.jpg","s22_L6x1_4.jpg","s22_L6x1_5.jpg","s22_L6x1_6.jpg"],
+// ["C",23,"40.sugar_high.jpg",33,"s23_L6x1_1.jpg","s23_L6x1_2.jpg","s23_L6x1_3.jpg","s23_L6x1_4.jpg","s23_L6x1_5.jpg","s23_L6x1_6.jpg"],
+// ["C",24,"53.piano_slide.jpg",33,"s24_L6x1_1.jpg","s24_L6x1_2.jpg","s24_L6x1_3.jpg","s24_L6x1_4.jpg","s24_L6x1_5.jpg","s24_L6x1_6.jpg"]
+// ];
+
+var AllDataSources=[DataSourceA,DataSourceB,DataSourceC];
