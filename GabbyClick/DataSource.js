0eb9342fcaaf04cb061792c9694601875129e6bf
foulsham <foulsham@essex.ac.uk>
2021-02-07 13:59:30 +0000

    adding files from home macbook

---
+++ GabbyClick/DataSource.js
@@ -0,0 +1,13 @@
+
+var DataSourceA = [
+["A",1,"Gabby1.jpg","right"],
+["A",2,"Gabby2.jpg","right"]
+];
+
+var DataSourceB = [
+["B",1,"Gabby3.jpg","right"],
+["B",2,"Gabby4.jpg","right"]
+];
+
+
+var AllDataSources=[DataSourceA,DataSourceB];
